cb24997048ac551e8d3009fea5bbcd276aa55d18
Malinda Mosholder <m.m.mosholder@gmail.com>
2017-07-31 21:47:40 -0600

    Update featured bourbon page, update nav

--- public/site/templates/featured-bourbon.php
+++ public/site/templates/featured-bourbon.php
@@ -2,36 +2,34 @@
 
 include('./_head.php'); // include header markup ?>
 
-	<div class="content-page contact">
+	<div class="content-page featured-bourbon">
 
 		<div class="container">
       <div class="-row">
-        <div id='content' class="content">
-          <!-- output 'headline' if available, otherwise 'title' -->
+        <div class="content">
           <h1><?=$page->get('headline|title')?></h1>
         </div>
       </div>
     </div>
 
-    <div class="container">
-      <div class="-row">
-        <div class="content"><?php
-          // output bodycopy
-          if($page->plain_text) {
-            echo "<div class='-text -center-align'><p>" . $page->plain_text . "</p></div>";
-          }
-         ?></div>
+    <?php if($page->plain_text) ?>
+      <div class="container">
+        <div class="-row">
+          <div class="content">
+            <div class='-text -center-align'><p><?=$page->plain_text?></p></div>
+           </div>
+         </div>
        </div>
-    </div>
 
     <div class="container">
       <div class="-row">
-        <div class="-full"><?php
-          foreach($page->featured_bourbon_items as $bourbon) {
-            $bg = $bourbon->images->first();
-            echo "<div class='-bourbon-item'><div class='-image' style='background-image: url('{$bg->url}')'></div><div class='-title'>{$bourbon->plain_text_title}</div><div class='-text'>{$bourbon->plain_text}</div></div>";
-          }
-        ?>
+        <div class="-full">
+          <?php foreach($page->featured_bourbon_items as $bourbon) ?>
+            <div class='-bourbon-item'>
+              <div class='-image' style='background-image: url(<?=$bourbon->images->first()->url?>)'></div>
+              <div class='-title'><?=$bourbon->plain_text_title?></div>
+              <div class='-text'><?=$bourbon->plain_text?></div>
+            </div>
         </div>
       </div>
     </div>
