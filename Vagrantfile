fc1bee5a4abe6004af7fdb2fa1290f27d56aaa02
Malinda Mosholder <m.m.mosholder@gmail.com>
2019-04-12 13:26:18 -0600

    updates

--- Vagrantfile
+++ Vagrantfile
@@ -3,41 +3,39 @@
 
 require File.dirname(__FILE__) + "/vagrant/addons/dependency_manager"
 
-check_plugins ["vagrant-exec", "vagrant-hostsupdater", "vagrant-triggers"]
+check_plugins ["vagrant-exec", "vagrant-hostmanager"]
 
 Vagrant.configure(2) do |config|
   # use scotchbox as base box
   config.vm.box = "scotch/box"
+  config.vm.box_version = '2.5';
 
   # set ip address to 192.168.33.10
-  config.vm.network "private_network", ip: "192.168.33.10"
+  config.vm.network "private_network", ip: "192.168.33.10", hostsupdater: "skip"
 
   # set project folder to /var/www
   config.vm.synced_folder ".", "/var/www", :mount_options => ["dmode=777", "fmode=777"]
 
   # set hostname
-  config.vm.hostname = "bluegrass.dev"
+  config.vm.hostname = "bluegrass.local"
+
+  # setup hostmanager
+  config.hostmanager.enabled = true
+  config.hostmanager.manage_host = true
+  config.hostmanager.manage_guest = true
+  config.hostmanager.ignore_private_ip = false
+  config.hostmanager.include_offline = true
 
   # make /var/www the working directory for all commands
   config.exec.commands '*', directory: '/var/www'
 
-  # install php 7
-  config.vm.provision "shell", path: "vagrant/provisioners/update-php.sh"
+  # config.trigger.after [:up, :reload, :provision] do |trigger|
+  #   trigger.info = "Creating test database..."
+  #   trigger.run_remote = {inline: "mysql -u root -proot -e 'CREATE DATABASE IF NOT EXISTS test;'"}
+  # end
 
-  config.trigger.after [:up, :reload, :provision] do
-    info "Starting mailcatcher..."
-    run "vagrant ssh -c \"pgrep mailcatch > /dev/null || /home/vagrant/.rbenv/shims/mailcatcher --http-ip=0.0.0.0\""
-
-    info "Installing composer packages..."
-    run "vagrant ssh -c \"cd /var/www ; composer install\""
-
-    info "Linking public storage directory..."
-    run "vagrant ssh -c \"cd /var/www ; php artisan storage:link -q\""
-
-    info "Creating test database..."
-    run "vagrant ssh -c \"mysql -e 'CREATE DATABASE IF NOT EXISTS test;'\""
-
-    info "Migrating database..."
-    run "vagrant ssh -c \"cd /var/www ; php artisan migrate\""
-  end
+  # config.trigger.after [:up, :reload, :provision] do |trigger|
+  #   trigger.info = "Installing composer packages..."
+  #   trigger.run_remote = {inline: "cd /var/www ; composer install"}
+  # end
 end
--- public/site/templates/home.php
+++ public/site/templates/home.php
@@ -1,37 +1,44 @@
 <?php
 
-include('./_head.php'); // include header markup ?>
+include('./_head.php'); 
+?>
 
-  <div class="home" style="background-image: url('<?php echo $config->urls->templates?>images/homepage-bg.jpg')">
-  	<div class="container">
-      <div class="-row">
-        <h1><?=$page->headline?></h1>
-        <div class="-sub">
-          <div><?=$page->home_subtitle?></div>
-          <div><?=$page->home_subtitle_2?></div>
-        </div>
+<div class="home" style="background-image: url('<?php echo $config->urls->templates ?>images/homepage-bg.jpg')">
+  <div class="container">
+    <div class="-row">
+      <h1><?= $page->headline ?></h1>
+      <div class="-sub">
+        <div><?= $page->home_subtitle ?></div>
+        <div><?= $page->home_subtitle_2 ?></div>
       </div>
-      <div class="-row -awards">
-        <div class="-award -first">
-          <div class="-title">"#1 Coffee Shop in Denver"</div>
-          <div class="-name">Denver A List</div>
-          <div class="-year">2017</div>
-        </div>
-        <div class="-award -center">
-          <div class="-title">"America's 80 Best Bourbon Bars"</div>
-          <div class="-name">The Bourbon Review</div>
-          <div class="-year">2016</div>
-        </div>
-        <div class="-award -last">
-          <div class="-title">"Best Pizza in Arvada"</div>
-          <div class="-name">Taste of Arvada</div>
-          <div class="-year">2015 & 2016</div>
-        </div>
+    </div>
+    <div class="-row -awards">
+      <div class="-award">
+        <div class="-title">"#1 Coffee Shop in Denver"</div>
+        <div class="-name">Denver A List</div>
+        <div class="-year">2017</div>
       </div>
-      <div class="-row">
-        <a href="/about" class="button -hollow-home">See What We're All About</a>
+      <div class="-award">
+        <div class="-title">"America's 80 Best Bourbon Bars"</div>
+        <div class="-name">The Bourbon Review</div>
+        <div class="-year">2016, 2017, & 2018</div>
       </div>
-  	</div>
+      <div class="-award">
+        <div class="-title">"Best Pizza in Arvada"</div>
+        <div class="-name">Taste of Arvada</div>
+        <div class="-year">2015 - 2018</div>
+      </div>
+      <div class="-award">
+        <div class="-title">"Best Savory Bit"</div>
+        <div class="-name">Taste of Arvada</div>
+        <div class="-year">2018</div>
+      </div>
+    </div>
+    <div class="-row">
+      <a href="/about" class="button -hollow-home">See What We're All About</a>
+    </div>
   </div>
+</div>
 
-<?php include('./_foot.php'); // include footer markup ?>
+<?php include('./_foot.php'); 
+?>
